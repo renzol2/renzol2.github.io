465fd05a0f29f89bb40a9d03c94244c161319129
Renzo <renzol2@illinois.edu>
2020-09-13 00:30:09 -0500

    Add chakra-ui and add face and greeting to landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default App;
