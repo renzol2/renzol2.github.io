643240abdec1435af11766dee8fa53aefef3a0a6
Renzo <renzol2@illinois.edu>
2020-12-28 15:01:54 -0600

    Add name as tab title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS>
+      <Head>
+        <title>Renzo Ledesma</title>
+      </Head>      
       <Component {...pageProps} />
     </ChakraProvider>
   );
