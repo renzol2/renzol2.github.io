894cc479869ea27874208607f94a3ab5b649cbd1
Renzo <renzol2@illinois.edu>
2021-07-17 13:20:39 -0500

    Update landing and packages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Box, Center, Heading, Text, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
+
 export default function Home() {
   return (
-    <h1>Hello</h1>    
+    <Center w="100%" h="100vh">
+      <Box>
+        <Heading textAlign="center" mb={5}>
+          Hi! 👋 I'm under construction.
+        </Heading>
+        <Text textAlign="center">
+          While I'm still being worked on, check out{' '}
+          <NextLink href="/is390" passHref>
+            <Link color="#D60270" fontWeight="bold">
+              this article I wrote on Asian representation in superhero comics.
+            </Link>
+          </NextLink>
+        </Text>
+      </Box>
+    </Center>
   );
 }
